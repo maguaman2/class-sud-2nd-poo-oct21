f8629e38dc066436a56ed71720adff34a15bffe7
Fabian <fgchillogalli@sudamericano.edu.ec>
2021-10-28 21:50:05 -0500

    Clase Docente

--- 01-fundamentos-poo/evaluaciones-practicas/m1-clases/src/Docente.java
+++ 01-fundamentos-poo/evaluaciones-practicas/m1-clases/src/Docente.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Docente {
+public class Docente {
+    private String id;
+    private String nombre;
+    private String profesion ;
+
+    public Docente(String id, String nombre, String profesion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.profesion = profesion;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getProfesion() {
+        return profesion;
+    }
+
+    public void setProfesion(String profesion) {
+        this.profesion = profesion;
+    }
 }
