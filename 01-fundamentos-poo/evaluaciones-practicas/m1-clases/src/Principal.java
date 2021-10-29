b15e5d859c7f44d4880a24cbca7d43e79cabb039
Darwin Tenecota <dftenecota@sudamericano.edu.ec>
2021-10-28 21:36:42 -0500

    Clase Provedor

--- 01-fundamentos-poo/evaluaciones-practicas/m1-clases/src/Principal.java
+++ 01-fundamentos-poo/evaluaciones-practicas/m1-clases/src/Principal.java
@@ -1,43 +1,13 @@
 public class Principal {
-/*
-    public static void main(String[] args) {
-        System.out.println("Pagina principal");
-        Producto producto = new Producto(1, "Producto",10);
-        producto.getDescripcion();
+    public static void main(int[] args) {
+        System.out.println("El proveedor es : ");
+        Provedor ProvedorNuevo = new Provedor("12345","Luis","");
 
-        Detalle detalle = new Detalle(1, 10,10);
-        detalle.getCantidad();
 
-        Factura factura = new Factura(1, "10/10/2021", "contado")
-        factura.getFecha();
 
-        Cliente cliente = new Cliente(1,"Juan", "Cuenca");
-        cliente.getNombre();
 
-        Proveedor proveedor = new Proveedor(1, "Proveedor", "2887581");
-        proveedor.getNombre();
 
-        Envio envio = new Envio(1, "Urgente");
-        envio.getDescipcion();
-
-        Estudiante estudiante = new Estudiante(1,"Pepito");
-        estudiante.getNombre();
-
-        Docente docente = new Docente (1, "Marco Aurelio");
-        docente.getNombre();
-
-        Asignatura asignatura = new Asignatura(1, "POO", 4);
-        asigntura.getDescipcion();
-
-        Pension pension = new Pension (1, "valor Octubre", 15);
-        pension.getDescription();
-
-        Notas notas = new Notas (1, 10);
-        notas,getNota();
-
-        Carrera carrera= new Carrera(1,"Desarrollo de software");
-        carrera.getDescipcion();
     }
 
- */
 }
+
